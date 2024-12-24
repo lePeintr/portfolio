4bea3706cf7f9dec8f5eb7f75c68622a680471f1
lePeintr <fodjotchuente@gmail.com>
2024-12-24 11:50:05 +0100

    fc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'skills', component: SkillsComponent },
+];
